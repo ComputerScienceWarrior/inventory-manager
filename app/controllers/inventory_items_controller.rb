1d650d7e170067df7b12fd5ea4d97fb234aa0538
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2026-08-01 12:17:13 -0700

    creates inital contorller action stubs

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -1,2 +1,40 @@
 class InventoryItemsController < ApplicationController
+    before_action :find_inventory_item, only: [:show]
+
+    def index
+        @invetory_items = InvnetoryItem.all
+    end
+
+    def show
+    end
+
+    def new
+        @inventory_item = InventoryItem.new
+    end
+
+    def create
+        
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def find_inventory_item
+        @inventory_item = InvnetoryItem.find(params[:id])
+    end
+
+    def inventory_item_params
+        params.require(:inventory_item).permit(:name);
+    end
 end
